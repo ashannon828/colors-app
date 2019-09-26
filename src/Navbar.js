45f10dd5e63c06281d89f1945b7172d05131738d
ashannon828 <ashannon828@gmail.com>
2019-09-26 11:46:55 +0300

    refactor navbar styles and remove css

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { Select, MenuItem, Snackbar, IconButton } from '@material-ui/core'
 import CloseIcon from '@material-ui/icons/Close'
-
+import { withStyles } from '@material-ui/styles'
 import Slider from 'rc-slider'
+
 import 'rc-slider/assets/index.css'
-import './Navbar.css'
+import styles from './styles/NavbarStyles'
 
 class Navbar extends Component {
 	constructor(props) {
@@ -22,17 +23,17 @@ class Navbar extends Component {
 		this.setState({ open: false })
 	}
 	render() {
-		const { level, changeLevel, showingAllColors } = this.props
+		const { level, changeLevel, showingAllColors, classes } = this.props
 		const { format } = this.state
 		return (
-			<header className="Navbar">
-				<div className="logo">
+			<header className={classes.Navbar}>
+				<div className={classes.logo}>
 					<Link to="/">reactcolorpicker</Link>
 				</div>
 				{showingAllColors && (
-					<div className="slider-container">
+					<div>
 						<span>Level: {level}</span>
-						<div className="slider">
+						<div className={classes.slider}>
 							<Slider
 								defaultValue={level}
 								min={100}
@@ -43,7 +44,7 @@ class Navbar extends Component {
 						</div>
 					</div>
 				)}
-				<div className="select-container">
+				<div className={classes.selectContainer}>
 					<Select value={format} onChange={this.handleFormatChange}>
 						<MenuItem value="hex">HEX - #ffffff</MenuItem>
 						<MenuItem value="rgb">RBG - rgb(255,255,255)</MenuItem>
@@ -77,4 +78,4 @@ class Navbar extends Component {
 	}
 }
 
-export default Navbar
+export default withStyles(styles)(Navbar)
